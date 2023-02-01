f6cd1aa3e69bcb3782bc569a6d772b6271c7345a
Trigah <ijenrandy2@gmail.com>
2023-02-01 01:22:02 +0100

    Get votes for a post using join SqlAlchemy

--- app/routers/post.py
+++ app/routers/post.py
@@ -19,6 +19,7 @@ def get_posts(db: session  = Depends(get_db), current_user : int = Depends(oauth
 @router.post("/posts",status_code=status.HTTP_201_CREATED,response_model=PostRespone)
 def create_posts(post: PostRequest, db: session  = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """ ,(post.title,post.content,post.published))
+
     new_post=models.Post(owner_id=current_user.id,**post.dict())
     db.add(new_post)
     db.commit()
@@ -28,6 +29,7 @@ def create_posts(post: PostRequest, db: session  = Depends(get_db), current_user
 @router.get("/posts/{id}",response_model=PostRespone)
 def get_post(id: int, response: Response, db: session  = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)): 
     # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id),))
+
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id {id} not found")  
@@ -41,6 +43,7 @@ def get_post(id: int, response: Response, db: session  = Depends(get_db), curren
 @router.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)    
 def delete_post(id: int,db: session  = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
