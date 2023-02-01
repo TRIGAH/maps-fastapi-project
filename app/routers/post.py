4b1a75cd196902716b8bc1713f3b3336739d000d
Trigah <ijenrandy2@gmail.com>
2023-02-01 01:20:53 +0100

    Get votes for a post

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,10 +10,11 @@ router = APIRouter(tags=["Posts"])
 @router.get("/posts",response_model=List[PostVotes])
 def get_posts(db: session  = Depends(get_db), current_user : int = Depends(oauth2.get_current_user),limit : int =2 ,skip : int =2, search : Optional[str]=""):
     # cursor.execute(""" SELECT * FROM posts """)
-    posts=db.query(models.Post).filter(models.Post.title.contains(search)).all()
 
-    results = db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
-    return results
+    posts = db.query(models.Post,func.count(models.Vote.post_id).label("votes"))\
+    .join(models.Vote,models.Vote.post_id == models.Post.id, isouter=True)\
+    .group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    return posts
 
 @router.post("/posts",status_code=status.HTTP_201_CREATED,response_model=PostRespone)
 def create_posts(post: PostRequest, db: session  = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
