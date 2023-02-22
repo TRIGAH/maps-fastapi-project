550c86fd313db2a6afa24b300ec0bcbb2ba86c7d
Trigah <ijenrandy2@gmail.com>
2023-02-22 02:10:02 +0100

    Pytest Create User and Login User tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,40 +1,33 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.config import settings
-from app.database import engine,get_db
-from app.database import Base
+from tests.database import client,session
+import pytest
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/fastapi_test'
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-Base.metadata.create_all(bind=engine)
+@pytest.fixture
+def test_user(client):
+    user_data = {"email":"maps@gmail.com","password":"maps"}
+    res=client.post("/users/",json=user_data)
+    assert res.status_code == 201
+    print (res.json())
+    new_user = res.json()
+    new_user["password"]=user_data["password"]
+    return new_user
 
 
-TestingSessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)  
 
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()  
 
-app.dependency_overrides[get_db] = override_get_db        
-
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json().get("message"))
     assert res.status_code == 200
     assert res.json().get("message") == "Welcome to My API"
 
-def test_create_user():
-    res = client.post("/users",json={"email":"test@gmail.com","password":"test123"})
+def test_create_user(client):
+    res = client.post("/users",json={"email":"test1@gmail.com","password":"test123"})
     new_user = schemas.UserResponse(**res.json())  
-    assert new_user.email == "test@gmail.com"  
+    assert new_user.email == "test1@gmail.com"  
     assert res.status_code == 201
+
+
+def test_login_user(client,test_user):
+    res = client.post("/login",data={"username":test_user["email"],"password":test_user["password"]})  
+    assert res.status_code == 200  
