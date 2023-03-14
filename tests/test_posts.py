c17d4b18883f943e0030a4af5fb51feaf3d30ea0
Trigah <ijenrandy2@gmail.com>
2023-03-14 05:22:38 +0100

    Tests for authorized and unauthorized users in retrieving posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -9,3 +9,24 @@ def test_get_posts(authorized_client,test_posts):
     posts_list = list(posts_map)
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+
+def test_unauthorized_user_get_posts(client,test_posts):    
+    res = client.get("/posts")
+    assert res.status_code == 401
+
+def test_unauthorized_user_get_post(client,test_posts):    
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_get_post_not_exist(authorized_client,test_posts):
+    res = authorized_client.get(f"/posts/888")
+    assert res.status_code == 404
+
+def test_authorized_user_get_post(authorized_client,test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")   
+    post = schemas.PostVotes(**res.json())
+    print(post)
+    assert post.Post.id == test_posts[0].id
+
+def test_create_post(authorized_client,test_user,test_posts):
+
